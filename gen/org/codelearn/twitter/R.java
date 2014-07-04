1f2642d043d36128190c63324402d62e7150d472
Suraj Dubey <sdubey.dubey14@gmail.com>
2014-07-04 20:53:31 +0530

    Commit

--- gen/org/codelearn/twitter/R.java
+++ gen/org/codelearn/twitter/R.java
@@ -1096,13 +1096,14 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050049;
+        public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int btn_login=0x7f050043;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int container=0x7f050044;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1119,7 +1120,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f050045;
+        public static final int imageView1=0x7f050049;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1148,12 +1149,14 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f050046;
-        public static final int textView2=0x7f050047;
-        public static final int textView3=0x7f050048;
+        public static final int textView2=0x7f05004a;
+        public static final int textView3=0x7f05004b;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int tweetList=0x7f050044;
+        public static final int tweetBody=0x7f050047;
+        public static final int tweetDate=0x7f050048;
+        public static final int tweetImage=0x7f050045;
+        public static final int tweetTitle=0x7f050046;
         public static final int uname_block=0x7f05003d;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
@@ -1211,13 +1214,15 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int activity_tweet_list=0x7f030019;
-        public static final int row_tweet=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int activity_tweet_detail=0x7f030019;
+        public static final int activity_tweet_list=0x7f03001a;
+        public static final int row_tweet=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
-        public static final int tweet_list=0x7f0c0001;
+        public static final int tweet_detail=0x7f0c0001;
+        public static final int tweet_list=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1270,6 +1275,7 @@ containing a value of this type.
         public static final int lbl_pwd=0x7f0a0013;
         public static final int lbl_username=0x7f0a0011;
         public static final int no_tweet_found=0x7f0a0017;
+        public static final int title_activity_tweet_detail=0x7f0a0018;
         public static final int title_activity_tweet_list=0x7f0a0016;
     }
     public static final class style {
--- src/org/codelearn/twitter/TweetListActivity.java
+++ src/org/codelearn/twitter/TweetListActivity.java
@@ -1,15 +1,17 @@
 package org.codelearn.twitter;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class TweetListActivity extends Activity {
+public class TweetListActivity extends ListActivity {
 	
 	private ListView tweetListView;
 	private String stringArray[];
-	private ArrayAdapter itemArrayAdapter;
+	private ArrayAdapter tweetItemArrayAdapter;
 	
 	
 
@@ -18,7 +20,7 @@ public class TweetListActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tweet_list);
 
-		tweetListView = (ListView) findViewById(R.id.tweetList);
+		//tweetListView = (ListView) findViewById(R.layout.id.);
 		
 		/*stringArray = new String[10];
 		for(int i=0;i<stringArray.length;i++)
@@ -29,8 +31,21 @@ public class TweetListActivity extends Activity {
 		itemArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stringArray);
 		tweetListView.setAdapter(itemArrayAdapter);*/
 		
-		itemArrayAdapter = new TweetAdapter(this, new String[10]);
-		tweetListView.setAdapter(itemArrayAdapter);
+		/*itemArrayAdapter = new TweetAdapter(this, new String[10]);
+		tweetListView.setAdapter(itemArrayAdapter);*/
+		
+		
+		tweetItemArrayAdapter = new TweetAdapter(this, new String [10]);
+		setListAdapter(tweetItemArrayAdapter);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		/*TextView t = (TextView) v.findViewById(R.id.tweetTitle);
+		t.setText("tweet Clicked!!");*/
+		
+		Intent intent = new Intent(getApplicationContext(), TweetDetailActivity.class);
+		startActivity(intent);
 	}
 
 
